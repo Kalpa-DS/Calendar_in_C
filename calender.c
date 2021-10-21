1f1a38d423bdfc70c7147db9a8c693815086476c
KavinduJayathilaka <70713220+KavinduJayathilaka@users.noreply.github.com>
2021-10-21 15:25:30 +0530

    Update calender.c

--- calender.c
+++ calender.c
@@ -154,6 +154,29 @@ char *getName(int day){
 
 //prints the name of month and year
 
+
+void print_date(int mm, int yy){ 
+    printf("---------------------------\n");
+    gotoxy(25,6);
+    switch(mm){
+        case 1: printf("January"); break;
+        case 2: printf("February"); break;
+        case 3: printf("March"); break;
+        case 4: printf("April"); break;
+        case 5: printf("May"); break;
+        case 6: printf("June"); break;
+        case 7: printf("July"); break;
+        case 8: printf("August"); break;
+        case 9: printf("September"); break;
+        case 10: printf("October"); break;
+        case 11: printf("November"); break;
+        case 12: printf("December"); break;
+    }
+    printf(" , %d", yy);
+    gotoxy(20,7);
+    printf("---------------------------");
+}
+}
 //*********
 
 
